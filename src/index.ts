71d31d856cde5760408071643d267ea667b92e4c
astorozhevsky <astorozhevsky@ronasit.com>
2020-07-01 15:32:39 +0600

    added ts + script

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { DocumentReference } from '@google-cloud/firestore';
+import {
+  TransformOperationExecutor
+} from 'class-transformer/TransformOperationExecutor';
+
+TransformOperationExecutor.prototype.transform = function (transform) {
+  return function (source: any, value: any, targetType: any, arrayType: any, isMap: any, level: any) {
+    if (value instanceof DocumentReference) {
+      return value;
+    }
+
+    // @ts-ignore
+    // tslint:disable-next-line:no-invalid-this
+    return transform.apply(this, [source, value, targetType, arrayType, isMap, level]);
+  };
+}(TransformOperationExecutor.prototype.transform);
