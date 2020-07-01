1d75a945d59375726cdc7fc81eae01cb56845726
astorozhevsky <astorozhevsky@ronasit.com>
2020-07-01 20:23:31 +0600

    added handling Timestamp

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { DocumentReference } from '@google-cloud/firestore';
+import { DocumentReference, Timestamp } from '@google-cloud/firestore';
 import {
   TransformOperationExecutor
 } from 'class-transformer/TransformOperationExecutor';
@@ -7,6 +7,10 @@ TransformOperationExecutor.prototype.transform = function (transform) {
   return function (source: any, value: any, targetType: any, arrayType: any, isMap: any, level: any) {
     if (value instanceof DocumentReference) {
       return value;
+    }
+
+    if (value instanceof Timestamp) {
+      return value.toDate();
     }
 
     // @ts-ignore
