7082cc3f8aa7c208014df5a7638f93e09b17166e
astorozhevsky <astorozhevsky@ronasit.com>
2020-07-08 14:55:46 +0600

    updated comparing + installed firebase module

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import { DocumentReference, Timestamp } from '@google-cloud/firestore';
+import { firestore } from 'firebase';
 import {
   TransformOperationExecutor
 } from 'class-transformer/TransformOperationExecutor';
 
 TransformOperationExecutor.prototype.transform = function (transform) {
   return function (source: any, value: any, targetType: any, arrayType: any, isMap: any, level: any) {
-    if (value instanceof DocumentReference) {
+    if (value instanceof DocumentReference || value instanceof firestore.DocumentReference) {
       return value;
     }
 
-    if (value instanceof Timestamp) {
+    if (value instanceof Timestamp || value instanceof firestore.Timestamp) {
       return value.toDate();
     }
 
